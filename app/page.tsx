ec98ea1ae3d8683b92319508ae762cf92598cd86
AGasior <aleksander.gasior@10clouds.com>
2024-02-03 17:11:56 +0100

    feat: create user store

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
 import Chat from '@/components/Chat';
 import Login from '@/components/Login';
+import InitUser from '@/lib/store/InitUser';
 import { supabaseServer } from '@/lib/supabase/server';
 
 export default async function Home() {
   const supabase = supabaseServer();
   const { data } = await supabase.auth.getSession();
+  const user = data.session?.user;
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {data.session?.user ? <Chat user={data.session?.user} /> : <Login />}
-    </main>
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        {user ? <Chat user={user} /> : <Login />}
+      </main>
+      <InitUser user={user} />
+    </>
   );
 }
